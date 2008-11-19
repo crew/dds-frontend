ed379e1c6e5ca33e3339f5d8f0f13b0886188fd5
Will Nowak <wan@ccs.neu.edu>
2008-11-19 21:12:43 +0000

    Donkeys like OO programming

--- dds.py
+++ dds.py
@@ -10,59 +10,62 @@ import xmpper
 import logging
 from optparse import OptionParser
 
-home = os.environ["HOME"]
-configFile = home + "/.dds/config.xml"
-logFile = home + "/.dds/log"
-cache = None
-
-stage = clutter.stage_get_default()
-p = None
 ## Setup stupid logging for the client
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s', )
 
-def parse_args():
-    parser = OptionParser(usage="usage: %prog [options]")
-    parser.add_option("-c", "--config", dest="config",
-                      help="location of the config file")
-    parser.add_option("-l", "--log", dest="log",
-                      help="location of the log file")
-    parser.add_option("-s", "--slides", dest="slides",
-                      help=("location of the slide cache directory "
-                            "(overides config file)"))
-    (options, args) = parser.parse_args()
-    if (options.config):
-        configFile = options.config
-    if (options.log):
-        logFile = options.log
-    if (options.slides):
-        cache = options.slides
+class DDS:
+    def __init__(self):
+        self._home = os.environ["HOME"]
+        self._configFile = home + "/.dds/config.xml"
+        self._logFile = home + "/.dds/log"
+        self._cache = None
+        self._stage = clutter.stage_get_default()
+        self._xmpp = None
 
-def on_key_press_event(stage, event):
-    logging.debug('Got keypress %s' % event.keyval)
-    if (event.keyval == 113):
-        clutter.main_quit()
+    def parse_args():
+        parser = OptionParser(usage="usage: %prog [options]")
+        parser.add_option("-c", "--config", dest="config",
+                          help="location of the config file")
+        parser.add_option("-l", "--log", dest="log",
+                          help="location of the log file")
+        parser.add_option("-s", "--slides", dest="slides",
+                          help=("location of the slide cache directory "
+                                "(overides config file)"))
+        (options, args) = parser.parse_args()
+        if (options.config):
+            self._configFile = options.config
+        if (options.log):
+            self._logFile = options.log
+        if (options.slides):
+            self._cache = options.slides
 
-def main(args):
-    gobject.threads_init()
-    clutter.threads_init()
-    parse_args()
-    config.init(configFile)
-    if (cache):
-        config.setOption("cache", cache)
-    stage.fullscreen()
-    stage.set_color(clutter.Color(0x00, 0x00, 0x00, 0x00))
-    stage.connect('destroy', clutter.main_quit)
-    stage.connect('key-press-event', on_key_press_event)
-    stage.hide_cursor()
-    stage.show_all()
-    show = slider.create(stage)
-    p = xmpper.create(show)
-    p.setupXmpp()
-    p.proceed()
-    gobject.timeout_add(5000, p.proceed)
-    clutter.main()
+    def on_key_press_event(stage, event):
+        logging.debug('Got keypress %s' % event.keyval)
+        if (event.keyval == 113):
+            clutter.main_quit()
+
+    def main(args):
+        gobject.threads_init()
+        clutter.threads_init()
+        self.parse_args()
+        config.init(self._configFile)
+        if (self._cache):
+            config.setOption("cache", cache)
+        self._stage.fullscreen()
+        self._stage.set_color(clutter.Color(0x00, 0x00, 0x00, 0x00))
+        self._stage.connect('destroy', clutter.main_quit)
+        self._stage.connect('key-press-event', on_key_press_event)
+        self._stage.hide_cursor()
+        self._stage.show_all()
+        self._show = slider.create(stage)
+        self._xmpp = xmpper.create(self._show)
+        self._xmpp.setupXmpp()
+        self._xmpp.proceed()
+        gobject.timeout_add(5000, self._xmpp.proceed)
+        clutter.main()
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    d = DDS()
+    d.main(sys.argv)
