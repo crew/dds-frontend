17d7e126d4e5c8d2f2ce44de9a623b18fe21b10b
Roderic Morris <roderic@ccs.neu.edu>
2008-11-22 22:04:05 +0000

    indent with two lines instead of four

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ import xml.etree.ElementTree as etree
 options = {}
 
 def init(file):
-    config = etree.parse(file)
-    root = config.getroot()
-    if(root.tag ==  "config"):
-        for element in root.getchildren():
-            options[element.get("name")] = element.get("value")
-    else:
-        raise Exception("Config file isn't valid")
+  config = etree.parse(file)
+  root = config.getroot()
+  if(root.tag ==  "config"):
+    for element in root.getchildren():
+      options[element.get("name")] = element.get("value")
+  else:
+    raise Exception("Config file isn't valid")
 
 def option(name):
-    return options[name]
+  return options[name]
 
 def setOption(name, value):
-    options[name] = value
+  options[name] = value
--- xmpper.py
+++ xmpper.py
@@ -12,120 +12,120 @@ from xml.sax.handler import ErrorHandler
 from threading import Thread
 
 def create(slider):
-    return Xmpper(slider)
+  return Xmpper(slider)
 
 
 class Xmpper(Thread):
 
-    def __init__(self, slider):
-        self.slider = slider
-        self.client = None
-        self.methods = { "addSlide"   : self.addSlide,
-                         "removeSlide" : self.removeSlide,
-                         "updateSlide" : self.updateSlide,
-                         "addAsset"    : self.addAsset,
-                         "removeAsset" : self.removeAsset,
-                         "updateAsset" : self.updateAsset }
-        Thread.__init__(self)
+  def __init__(self, slider):
+    self.slider = slider
+    self.client = None
+    self.methods = { "addSlide"   : self.addSlide,
+                     "removeSlide" : self.removeSlide,
+                     "updateSlide" : self.updateSlide,
+                     "addAsset"    : self.addAsset,
+                     "removeAsset" : self.removeAsset,
+                     "updateAsset" : self.updateAsset }
+    Thread.__init__(self)
 
-    def run(self):
-        self.setupXmpp()
+  def run(self):
+    self.setupXmpp()
 
-    def addSlide(self, slide):
-        logging.debug('adding slide to the pot')
+  def addSlide(self, slide):
+    logging.debug('adding slide to the pot')
         #slide[0] has a hash with the id, duration, and priority of the slide
         #slide[1] has a list of hashes, where each hash has the url and id of
         #an asset
-        info = slide[0]
-        assets = slide[1]
-        configdirectory = config.option("cache") + "/" + str(info["id"])
-	directory = os.path.expanduser(configdirectory)
-        if not os.path.exists(directory):
-            os.mkdir(directory)
-        for asset in assets:
-            path = urlparse.urlparse(asset["url"])[2]
-            name = os.path.basename(path)
-            fullPath = directory + "/" + name
-            urllib.urlretrieve(asset["url"], fullPath)
-        info["assets"] = assets
-        info["directory"] = directory
-        flag = self.slider.isEmpty()
-        self.slider.addSlide(**info)
-        if flag:
-            logging.debug('starting slider')
-            self.slider.start()
+    info = slide[0]
+    assets = slide[1]
+    configdirectory = config.option("cache") + "/" + str(info["id"])
+    directory = os.path.expanduser(configdirectory)
+    if not os.path.exists(directory):
+      os.mkdir(directory)
+    for asset in assets:
+      path = urlparse.urlparse(asset["url"])[2]
+      name = os.path.basename(path)
+      fullPath = directory + "/" + name
+      urllib.urlretrieve(asset["url"], fullPath)
+    info["assets"] = assets
+    info["directory"] = directory
+    flag = self.slider.isEmpty()
+    self.slider.addSlide(**info)
+    if flag:
+      logging.debug('starting slider')
+      self.slider.start()
 
-    def removeSlide(self, slide):
-        logging.debug("removing a slide")
-        info = slide[0]
-        self.slider.removeSlide(info["id"])
-        if self.slider.isEmpty():
-            slider.stop()
+  def removeSlide(self, slide):
+    logging.debug("removing a slide")
+    info = slide[0]
+    self.slider.removeSlide(info["id"])
+    if self.slider.isEmpty():
+      slider.stop()
 
-    def updateSlide(self, slide):
-        logging.debug('Update slide: %s' % slide)
-        removeSlide(slide)
-        addSlide(slide)
+  def updateSlide(self, slide):
+    logging.debug('Update slide: %s' % slide)
+    removeSlide(slide)
+    addSlide(slide)
 
-    def addAsset(self, slide):
-        logging.debug('Add Asset: %s' % slide)
+  def addAsset(self, slide):
+    logging.debug('Add Asset: %s' % slide)
 
-    def removeAsset(self, slide):
-        logging.debug('Remove Asset: %s' % slide)
+  def removeAsset(self, slide):
+    logging.debug('Remove Asset: %s' % slide)
 
-    def updateAsset(self, slide):
-        logging.debug('Update Asset: %s' % slide)
+  def updateAsset(self, slide):
+    logging.debug('Update Asset: %s' % slide)
 
-    def handlePresence(self, dispatch, pr):
-        jid = pr.getAttr('from')
-        logging.debug('Handle Presence From: %s' % jid)
-        dispatch.send(Presence(jid, 'subscribed'))
+  def handlePresence(self, dispatch, pr):
+    jid = pr.getAttr('from')
+    logging.debug('Handle Presence From: %s' % jid)
+    dispatch.send(Presence(jid, 'subscribed'))
 
-    def handleIQ(self, connection, iq):
-        if iq.getQueryNS() == NS_RPC:
-            if iq.getAttr("type") == "error":
-                self.logError(iq.getAttr("from"), "rpc error")
-            else:
-                payload = xmlrpclib.loads(str(iq))
-                logging.debug('Payload: %s' % str(payload))
-                methodName = payload[1]
-                # payload[0] returns a tuple of arguments
-                # and the only argument we want is the first one
-                if methodName in self.methods:
-                    self.methods[methodName](payload[0])
-                else:
-                    logging.error("rpc function " + methodName +
-                                  " is not defined")
+  def handleIQ(self, connection, iq):
+    if iq.getQueryNS() == NS_RPC:
+      if iq.getAttr("type") == "error":
+        self.logError(iq.getAttr("from"), "rpc error")
+      else:
+        payload = xmlrpclib.loads(str(iq))
+        logging.debug('Payload: %s' % str(payload))
+        methodName = payload[1]
+        # payload[0] returns a tuple of arguments
+        # and the only argument we want is the first one
+        if methodName in self.methods:
+          self.methods[methodName](payload[0])
+        else:
+          logging.error("rpc function " + methodName +
+                        " is not defined")
 
-        raise NodeProcessed
+    raise NodeProcessed
 
-    def checkXmpp(self, connection):
-        try:
-            connection.Process(1)
-            return True
-        except KeyboardInterrupt:
-            return False
+  def checkXmpp(self, connection):
+    try:
+      connection.Process(1)
+      return True
+    except KeyboardInterrupt:
+      return False
 
-    def proceed(self):
-        while self.checkXmpp(self.client):
-            pass
+  def proceed(self):
+    while self.checkXmpp(self.client):
+      pass
 
-    def setupXmpp(self):
-        jid = config.option("client-jid")
-        password = config.option("client-password")
+  def setupXmpp(self):
+    jid = config.option("client-jid")
+    password = config.option("client-password")
 
-        jid = protocol.JID(jid)
-        client = Client(jid.getDomain(), debug=[])
+    jid = protocol.JID(jid)
+    client = Client(jid.getDomain(), debug=[])
 
-        if client.connect() == "":
-            logging.error("Could not connection to the XMPP server")
+    if client.connect() == "":
+      logging.error("Could not connection to the XMPP server")
 
-        if client.auth(jid.getNode(), password, jid.getResource()) == None:
-            logging.error("XMPP password was incorrect")
+    if client.auth(jid.getNode(), password, jid.getResource()) == None:
+      logging.error("XMPP password was incorrect")
 
-        client.RegisterHandler("iq", self.handleIQ)
-        client.RegisterHandler("presense", self.handlePresence)
-        client.sendInitPresence()
-        client.sendPresence(jid = config.option("server-jid"))
-        self.client = client
-        self.proceed()
+    client.RegisterHandler("iq", self.handleIQ)
+    client.RegisterHandler("presense", self.handlePresence)
+    client.sendInitPresence()
+    client.sendPresence(jid = config.option("server-jid"))
+    self.client = client
+    self.proceed()
