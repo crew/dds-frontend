77cb10fd5bbfb0c952aa6d3a8bb7d0d7a3c3286b
Roderic Morris <roderic@ccs.neu.edu>
2008-11-22 22:04:05 +0000

    indent with two lines instead of four

--- dds.py
+++ dds.py
@@ -15,66 +15,66 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s', )
 
 class DDS:
-    def __init__(self):
-        self._home = os.environ["HOME"]
-        self._configFile = self._home + "/.dds/config.xml"
-        self._logFile = self._home + "/.dds/log"
-        self._cache = None
-        self._stage = clutter.stage_get_default()
-        self._xmpp = None
+  def __init__(self):
+    self._home = os.environ["HOME"]
+    self._configFile = self._home + "/.dds/config.xml"
+    self._logFile = self._home + "/.dds/log"
+    self._cache = None
+    self._stage = clutter.stage_get_default()
+    self._xmpp = None
 
-    def parse_args(self):
-        logging.debug('Parsing Args')
-        parser = OptionParser(usage="usage: %prog [options]")
-        parser.add_option("-c", "--config", dest="config",
-                          help="location of the config file")
-        parser.add_option("-l", "--log", dest="log",
-                          help="location of the log file")
-        parser.add_option("-s", "--slides", dest="slides",
-                          help=("location of the slide cache directory "
-                                "(overides config file)"))
-        (options, args) = parser.parse_args()
-        if (options.config):
-            self._configFile = options.config
-        if (options.log):
-            self._logFile = options.log
-        if (options.slides):
-            self._cache = options.slides
+  def parse_args(self):
+    logging.debug('Parsing Args')
+    parser = OptionParser(usage="usage: %prog [options]")
+    parser.add_option("-c", "--config", dest="config",
+                      help="location of the config file")
+    parser.add_option("-l", "--log", dest="log",
+                      help="location of the log file")
+    parser.add_option("-s", "--slides", dest="slides",
+                      help=("location of the slide cache directory "
+                            "(overides config file)"))
+    (options, args) = parser.parse_args()
+    if (options.config):
+      self._configFile = options.config
+    if (options.log):
+      self._logFile = options.log
+    if (options.slides):
+      self._cache = options.slides
 
-    def on_key_press_event(self,stage, event):
-        logging.debug('Got keypress %s' % event.keyval)
-        if (event.keyval == 113):
-            clutter.main_quit()
-        elif (event.keyval == 65365):
-            self._show.next()
+  def on_key_press_event(self,stage, event):
+    logging.debug('Got keypress %s' % event.keyval)
+    if (event.keyval == 113):
+      clutter.main_quit()
+    elif (event.keyval == 65365):
+      self._show.next()
 
-    def main(self,args):
-        logging.debug('Main method turn on!')
-        gobject.threads_init()
-        clutter.threads_init()
-        self.parse_args()
-        config.init(self._configFile)
-        if (self._cache):
-            config.setOption("cache", cache)
-        logging.debug('Going Fullscreen')
-        self._stage.fullscreen()
-        self._stage.set_color(clutter.Color(0x00, 0x00, 0x00, 0x00))
-        self._stage.connect('destroy', clutter.main_quit)
-        self._stage.connect('key-press-event', self.on_key_press_event)
-        self._stage.hide_cursor()
-        self._stage.show_all()
-        logging.debug('Creating slider')
-        self._show = slider.create(self._stage)
-        logging.debug('Creating xmpper')
-        self._xmpp = xmpper.create(self._show)
-        logging.debug('Starting xmpper')
-        self._xmpp.start()
-        logging.debug('Clutter Main invocation')
-        clutter.main()
+  def main(self,args):
+    logging.debug('Main method turn on!')
+    gobject.threads_init()
+    clutter.threads_init()
+    self.parse_args()
+    config.init(self._configFile)
+    if (self._cache):
+      config.setOption("cache", cache)
+    logging.debug('Going Fullscreen')
+    self._stage.fullscreen()
+    self._stage.set_color(clutter.Color(0x00, 0x00, 0x00, 0x00))
+    self._stage.connect('destroy', clutter.main_quit)
+    self._stage.connect('key-press-event', self.on_key_press_event)
+    self._stage.hide_cursor()
+    self._stage.show_all()
+    logging.debug('Creating slider')
+    self._show = slider.create(self._stage)
+    logging.debug('Creating xmpper')
+    self._xmpp = xmpper.create(self._show)
+    logging.debug('Starting xmpper')
+    self._xmpp.start()
+    logging.debug('Clutter Main invocation')
+    clutter.main()
 
 
 if __name__ == '__main__':
-    d = DDS()
-    retcode = d.main(sys.argv)
-    print retcode
-    sys.exit(retcode)
+  d = DDS()
+  retcode = d.main(sys.argv)
+  print retcode
+  sys.exit(retcode)
--- slider.py
+++ slider.py
@@ -18,281 +18,281 @@ from xml.sax.handler import ErrorHandler
 
 
 def create(canvas):
-    """Public creator for the slider"""
+  """Public creator for the slider"""
 
-    return Slider(canvas)
+  return Slider(canvas)
 
 class Slide(Group):
 
-    def __init__(self, transition):
-        self.transition = transition
-        Group.__init__(self)
+  def __init__(self, transition):
+    self.transition = transition
+    Group.__init__(self)
 
 class LayoutHandler(ContentHandler):
 
-    def __init__(self, stage, directory):
-        self.stage = stage
-        self.directory = os.path.expanduser(directory)
-        self.locator = None
-        self.slide = None
-        self.label = None
-        self.image = None
-        self.video = None
+  def __init__(self, stage, directory):
+    self.stage = stage
+    self.directory = os.path.expanduser(directory)
+    self.locator = None
+    self.slide = None
+    self.label = None
+    self.image = None
+    self.video = None
 
-        ContentHandler.__init__(self)
+    ContentHandler.__init__(self)
 
-    def setDocumentLocator(self, locator):
-        """Set the locator for this document"""
+  def setDocumentLocator(self, locator):
+    """Set the locator for this document"""
 
-        self.locator = locator
+    self.locator = locator
 
-    def startDocument(self):
-        """Handle the start of the xml document"""
+  def startDocument(self):
+    """Handle the start of the xml document"""
 
-        pass
+    pass
 
-    def startElement(self, name, attrs):
-        """Handle the start of an xml element"""
+  def startElement(self, name, attrs):
+    """Handle the start of an xml element"""
 
-        if (self.slide is None) and (name != "slide"):
+    if (self.slide is None) and (name != "slide"):
             #TODO: handle error
-            pass
+      pass
 
-        elif name == "slide":
-            transition = attrs.get("transition")
-            if transition is None:
-                pass
+    elif name == "slide":
+      transition = attrs.get("transition")
+      if transition is None:
+        pass
                 #TODO: handle error
-            self.slide = Slide(transition)
+      self.slide = Slide(transition)
 
-        elif name == "text":
-            logging.debug('Adding text element')
-            label = Label()
-            label.set_font_name(attrs.get("font", "sans 32"))
-            label.set_line_wrap(True)
-            label.set_color(clutter.color_parse(attrs.get("color", "white")))
-            labelWidth = (self.stage.get_width() / 16) * int(attrs.get("width", 16))
-            label.set_width(labelWidth)
-            labelHeight = (self.stage.get_height() / 9) * int(attrs.get("height", 9))
-            label.set_height(labelHeight)
-            labelX = (self.stage.get_width() / 16) * int(attrs.get("x", 0))
-            label.set_x(labelX)
-            labelY = (self.stage.get_height() / 9) * int(attrs.get("y",0))
-            label.set_y(labelY)
-            label.set_depth(int(attrs.get("z", 0)))
-            label.set_text("")
-            self.label = label
+    elif name == "text":
+      logging.debug('Adding text element')
+      label = Label()
+      label.set_font_name(attrs.get("font", "sans 32"))
+      label.set_line_wrap(True)
+      label.set_color(clutter.color_parse(attrs.get("color", "white")))
+      labelWidth = (self.stage.get_width() / 16) * int(attrs.get("width", 16))
+      label.set_width(labelWidth)
+      labelHeight = (self.stage.get_height() / 9) * int(attrs.get("height", 9))
+      label.set_height(labelHeight)
+      labelX = (self.stage.get_width() / 16) * int(attrs.get("x", 0))
+      label.set_x(labelX)
+      labelY = (self.stage.get_height() / 9) * int(attrs.get("y",0))
+      label.set_y(labelY)
+      label.set_depth(int(attrs.get("z", 0)))
+      label.set_text("")
+      self.label = label
 
-        elif name == "image":
-            logging.debug('Adding image element')
-            image = Texture()
-            imageWidth = (self.stage.get_width() / 16) * int(attrs.get("width", 16))
-            image.set_width(imageWidth)
-            imageHeight = (self.stage.get_height() / 9) * int(attrs.get("height", 9))
-            image.set_height(imageHeight)
-            imageX = (self.stage.get_width() / 16) * int(attrs.get("x", 0))
-            image.set_x(imageX)
-            imageY = (self.stage.get_height() / 9) * int(attrs.get("y", 0))
-            image.set_y(imageY)
-            image.set_depth(int(attrs.get("z", 0)))
-            self.image = image
+    elif name == "image":
+      logging.debug('Adding image element')
+      image = Texture()
+      imageWidth = (self.stage.get_width() / 16) * int(attrs.get("width", 16))
+      image.set_width(imageWidth)
+      imageHeight = (self.stage.get_height() / 9) * int(attrs.get("height", 9))
+      image.set_height(imageHeight)
+      imageX = (self.stage.get_width() / 16) * int(attrs.get("x", 0))
+      image.set_x(imageX)
+      imageY = (self.stage.get_height() / 9) * int(attrs.get("y", 0))
+      image.set_y(imageY)
+      image.set_depth(int(attrs.get("z", 0)))
+      self.image = image
 
-        elif name == "video":
+    elif name == "video":
             #TODO: implement this
-            pass
+      pass
 
-    def endElement(self, name):
+  def endElement(self, name):
 
-        if name == "text":
-            self.slide.add(self.label)
-            self.label = None
+    if name == "text":
+      self.slide.add(self.label)
+      self.label = None
 
-        if name == "image":
-            self.slide.add(self.image)
-            self.image = None
+    if name == "image":
+      self.slide.add(self.image)
+      self.image = None
 
-        if name == "video":
+    if name == "video":
             #TODO: implement
-            pass
+      pass
 
-    def characters(self, content):
+  def characters(self, content):
 
-        if not (self.label is None):
-            logging.debug('setting text label: %s' % content)
-            self.label.set_text(label.get_text() + content)
+    if not (self.label is None):
+      logging.debug('setting text label: %s' % content)
+      self.label.set_text(label.get_text() + content)
 
-        elif not (self.image is None):
-            file = self.directory + "/" + content
-            logging.debug('setting image src: %s' % file)
-            self.image.set_from_file(file)
+    elif not (self.image is None):
+      file = self.directory + "/" + content
+      logging.debug('setting image src: %s' % file)
+      self.image.set_from_file(file)
 
-        elif not (self.video is None):
+    elif not (self.video is None):
             #TODO: implement
-            pass
+      pass
 
 
 class Slideshow():
-    """Slideshow class
+  """Slideshow class
 
-    """
+  """
 
-    slides = []
+  slides = []
 
-    def __init__(self, stage):
+  def __init__(self, stage):
 
-        self.stage = stage
-        self.current = None
-        self.last = None
+    self.stage = stage
+    self.current = None
+    self.last = None
 
-    def isEmpty(self):
-        return len(self.slides) == 0
+  def isEmpty(self):
+    return len(self.slides) == 0
 
-    def parseLayout(self, file, directory):
-        """Parses the given file into a slide"""
-        logging.debug('Parsing layout file: %s dir: %s' % (file, directory))
-        handler = LayoutHandler(self.stage, directory)
-        sax.parse(file, handler)
-        return handler.slide
+  def parseLayout(self, file, directory):
+    """Parses the given file into a slide"""
+    logging.debug('Parsing layout file: %s dir: %s' % (file, directory))
+    handler = LayoutHandler(self.stage, directory)
+    sax.parse(file, handler)
+    return handler.slide
 
-    def addSlide(self, id, duration, priority, assets, directory):
-        logging.debug('Adding New Slide')
-        """Add a new slide to the interal cache"""
+  def addSlide(self, id, duration, priority, assets, directory):
+    """Add a new slide to the interal cache"""
 
-        slide = self.parseLayout(directory + "/layout.xml", directory)
-        slide.___id = id
-        slide.duration = duration
-        slide.priority = priority
-        self.slides.append(slide)
-        if not self.current:
-            self.current = self.currentSlide()
+    logging.debug('Adding New Slide')
+    slide = self.parseLayout(directory + "/layout.xml", directory)
+    slide.___id = id
+    slide.duration = duration
+    slide.priority = priority
+    self.slides.append(slide)
+    if not self.current:
+      self.current = self.currentSlide()
 
-    def nextSlide(self):
-        """Rotate the next slide to the front of the list"""
+  def nextSlide(self):
+    """Rotate the next slide to the front of the list"""
 
-        self.slides.append(self.slides.pop(0))
+    self.slides.append(self.slides.pop(0))
 
-    def currentSlide(self):
-        """Return the current slide"""
+  def currentSlide(self):
+    """Return the current slide"""
 
-        return self.slides[0]
+    return self.slides[0]
 
-    def next(self):
-        self.load_next()
-        self.paint()
+  def next(self):
+    self.load_next()
+    self.paint()
 
-    def setup_animation(self):
-        logging.debug('Setting up animation')
-        if(self.current.transition == "fade"):
-            self.current.set_opacity(0)
-        elif(self.current.transition == "slide-right-left"):
-            self.current.set_x(0 - self.stage.get_width())
-        elif(self.current.transition == "slide-left-right"):
-            self.current.set_x(self.stage.get_width())
-        elif(self.current.transition == "slide-up-down"):
-            self.current.set_y(0 - self.stage.get_height())
-        elif(self.current.transition == "slide-down-up"):
-            self.current.set_y(self.stage.get_height())
+  def setup_animation(self):
+    logging.debug('Setting up animation')
+    if(self.current.transition == "fade"):
+      self.current.set_opacity(0)
+    elif(self.current.transition == "slide-right-left"):
+      self.current.set_x(0 - self.stage.get_width())
+    elif(self.current.transition == "slide-left-right"):
+      self.current.set_x(self.stage.get_width())
+    elif(self.current.transition == "slide-up-down"):
+      self.current.set_y(0 - self.stage.get_height())
+    elif(self.current.transition == "slide-down-up"):
+      self.current.set_y(self.stage.get_height())
 
-    def in_animation(self):
-        logging.debug('in animation')
-        timeline = clutter.Timeline(fps=60, duration=500)
-        template = clutter.EffectTemplate(timeline, clutter.sine_inc_func)
-        effect = None
-        if(self.current.transition == "fade"):
-            effect = clutter.effect_fade(template, self.current, 255)
-        elif((self.current.transition == "slide-right-left") or
-             (self.current.transition == "slide-left-right") or
-             (self.current.transition == "slide-up-down") or
-             (self.current.transition == "slide-down-up")):
-            effect = clutter.effect_move(template, self.current, 0, 0)
+  def in_animation(self):
+    logging.debug('in animation')
+    timeline = clutter.Timeline(fps=60, duration=500)
+    template = clutter.EffectTemplate(timeline, clutter.sine_inc_func)
+    effect = None
+    if(self.current.transition == "fade"):
+      effect = clutter.effect_fade(template, self.current, 255)
+    elif((self.current.transition == "slide-right-left") or
+         (self.current.transition == "slide-left-right") or
+         (self.current.transition == "slide-up-down") or
+         (self.current.transition == "slide-down-up")):
+      effect = clutter.effect_move(template, self.current, 0, 0)
 
-        if(effect):
-            effect.start()
+    if(effect):
+      effect.start()
 
-    def out_animation(self):
-        logging.debug('out animation')
-        timeline = clutter.Timeline(fps=60, duration=500)
-        template = clutter.EffectTemplate(timeline, clutter.sine_inc_func)
-        effect = None
-        if(self.current.transition == "fade"):
-            effect = clutter.effect_fade(template, self.current, 0)
-        elif(self.current.transition == "slide-right-left"):
-            effect = clutter.effect_move(template, self.current,
-                                         self.stage.get_width(), 0)
-        elif(self.current.transition == "slide-left-right"):
-            effect = clutter.effect_move(template, self.current,
-                                         0 - self.stage.get_width(), 0)
-        elif(self.current.transition == "slide-up-down"):
-            effect = clutter.effect_move(template, self.current,
-                                         0, self.stage.get_height())
-        elif(self.current.transition == "slide-down-up"):
-            effect = clutter.effect_move(template, self.current,
-                                         0, 0 - self.stage.get_height())
+  def out_animation(self):
+    logging.debug('out animation')
+    timeline = clutter.Timeline(fps=60, duration=500)
+    template = clutter.EffectTemplate(timeline, clutter.sine_inc_func)
+    effect = None
+    if(self.current.transition == "fade"):
+      effect = clutter.effect_fade(template, self.current, 0)
+    elif(self.current.transition == "slide-right-left"):
+      effect = clutter.effect_move(template, self.current,
+                                   self.stage.get_width(), 0)
+    elif(self.current.transition == "slide-left-right"):
+      effect = clutter.effect_move(template, self.current,
+                                   0 - self.stage.get_width(), 0)
+    elif(self.current.transition == "slide-up-down"):
+      effect = clutter.effect_move(template, self.current,
+                                   0, self.stage.get_height())
+    elif(self.current.transition == "slide-down-up"):
+      effect = clutter.effect_move(template, self.current,
+                                   0, 0 - self.stage.get_height())
 
-        if(effect):
-            effect.start()
+    if(effect):
+      effect.start()
 
-    def load_next(self):
-        """Prepare the next slide to be painted"""
-        logging.debug('load_next')
+  def load_next(self):
+    """Prepare the next slide to be painted"""
+    logging.debug('load_next')
 
-        if len(self.slides) > 1:
-            self.out_animation()
-            if self.last:
-                self.last.hide_all()
-                self.stage.remove(self.last)
-            self.last = self.current
-            self.nextSlide()
-            self.current = self.currentSlide()
-            self.setup_animation()
+    if len(self.slides) > 1:
+      self.out_animation()
+      if self.last:
+        self.last.hide_all()
+        self.stage.remove(self.last)
+      self.last = self.current
+      self.nextSlide()
+      self.current = self.currentSlide()
+      self.setup_animation()
 
-    def paint(self):
-        """Paint the next slide to the screen"""
-        logging.debug('paint method begin')
-        if self.current:
-            logging.debug('painting')
-            self.in_animation()
-            self.current.show_all()
-            self.stage.add(self.current)
+  def paint(self):
+    """Paint the next slide to the screen"""
+    logging.debug('paint method begin')
+    if self.current:
+      logging.debug('painting')
+      self.in_animation()
+      self.current.show_all()
+      self.stage.add(self.current)
 
 class Slider(Slideshow):
-    """Manages the order and timing of slide switching"""
+  """Manages the order and timing of slide switching"""
 
-    def __init__(self, canvas):
-        self.timer = None
-        self.active = False
-        Slideshow.__init__(self, canvas)
+  def __init__(self, canvas):
+    self.timer = None
+    self.active = False
+    Slideshow.__init__(self, canvas)
 
-    def start(self):
-        """Starts the Slideshow"""
-        logging.debug('starting slider')
-        self.active = True
-        self.setup_animation()
-        self.reset_timer()
-        self.paint()
+  def start(self):
+    """Starts the Slideshow"""
+    logging.debug('starting slider')
+    self.active = True
+    self.setup_animation()
+    self.reset_timer()
+    self.paint()
 
-    def stop(self):
-        """Stops the Slideshow"""
-        logging.debug('stopping slider')
+  def stop(self):
+    """Stops the Slideshow"""
+    logging.debug('stopping slider')
 
-        self.active = False
-        if self.timer:
-            self.timer = None
+    self.active = False
+    if self.timer:
+      self.timer = None
 
-    def reset_timer(self):
-        """Runs the next timer thread to change slides"""
+  def reset_timer(self):
+    """Runs the next timer thread to change slides"""
 
-        if self.timer:
-            self.timer = None
+    if self.timer:
+      self.timer = None
 
-        if self.active:
-            self.timer = Timer(float(self.currentSlide().duration),
-                               self.next)
-            self.timer.daemon = True
-            self.timer.start()
+    if self.active:
+      self.timer = Timer(float(self.currentSlide().duration),
+                         self.next)
+      self.timer.daemon = True
+      self.timer.start()
 
-    def next(self):
-        """Runs the timer thread for, and shows the next slide"""
+  def next(self):
+    """Runs the timer thread for, and shows the next slide"""
 
-        Slideshow.next(self)
-        self.reset_timer()
+    Slideshow.next(self)
+    self.reset_timer()
