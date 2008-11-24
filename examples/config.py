ecb47693b709849df5aa951ca0f07d839af8e99c
Roderic Morris <roderic@ccs.neu.edu>
2008-11-24 17:15:29 +0000

    add a new example config, uses python code for configuration

---
+++ examples/config.py
@@ -0,0 +1,4 @@
+config = { "cache" : "/home/roderic/.dds/slides",
+           "client-jid" : "rms@centipede.ccs.neu.edu/dds-client",
+           "client-password" : "foo",
+           "server-jid" : "test@centipede.ccs.neu.edu/dds-server-init" }
