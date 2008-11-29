a64e68bd2af286394adf2f60b5580fbfda366970
Roderic Morris <roderic@ccs.neu.edu>
2008-11-29 18:06:15 +0000

    add a stub for remove slide, fix the example config file

--- examples/config.py
+++ examples/config.py
@@ -1,4 +1,4 @@
-config = { "cache" : "/home/roderic/.dds/slides",
+config = { "cache" : "~/.dds/slides",
            "client-jid" : "rms@centipede.ccs.neu.edu/dds-client",
            "client-password" : "foo",
            "server-jid" : "test@centipede.ccs.neu.edu/dds-server-init" }
