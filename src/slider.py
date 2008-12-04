64b51136ae2e4cca063bdf3764be96ea2431704e
Will Nowak <wan@ccs.neu.edu>
2008-12-04 09:07:41 +0000

    Switch to a gobject timer instead of threading

--- src/slider.py
+++ src/slider.py
@@ -3,14 +3,13 @@ import config
 import urllib
 import os
 import logging
-from threading import Timer
+import gobject
 from slideshow import Slideshow
 
 class Slider(Slideshow):
   """Manages the order and timing of slide switching"""
 
   def __init__(self, canvas, letterbox=False):
-    self.timer = None
     self.active = False
     Slideshow.__init__(self, canvas, letterbox=letterbox)
 
@@ -26,22 +25,13 @@ class Slider(Slideshow):
     """Stops the Slideshow"""
     logging.debug('slider stop')
     self.active = False
-    if self.timer:
-      self.timer = None
 
   def reset_timer(self):
     """Runs the next timer thread to change slides"""
     logging.debug('slider reset_timer')
 
-    if self.timer:
-      self.timer = None
-
-    if self.active:
-      self.timer = Timer(float(self.currentSlide().duration),
-                         self.next)
-      self.timer.daemon = True
-      self.timer.start()
-      logging.debug('slider reset_timer new timer start')
+    gobject.timeout_add(1000*self.currentSlide().duration, self.next)
+    logging.debug('slider reset_timer new timer start')
 
   def next(self):
     """Runs the timer thread for, and shows the next slide"""
