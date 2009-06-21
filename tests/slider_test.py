5a2df5ad2347cd1ac64fc348060b62f67d7ab18d
Roderic Morris <roderic@ccs.neu.edu>
2009-06-21 14:25:06 -0400

    Get the few tests we have working again.

--- tests/slider_test.py
+++ tests/slider_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import clutter
 import config
-from slider import Slider
+import slider
 from unittest import TestCase
 
 CACHE = "./cache"
@@ -9,19 +9,27 @@ CACHE = "./cache"
 class SliderTest(TestCase):
 
   def setUp(self):
-    self.slider = Slider(clutter.Stage())
+    self.slider = slider.Slider(clutter.Stage())
     config.setOption("cache", CACHE)
-    self.slider.addSlide(1, 3, 1)
-    self.slider.addSlide(2, 3, 1)
-    self.slider.addSlide(3, 3, 1)
+    self.slider.addSlide({'id': 1, 'duration': 3,
+                          'mode': 'layout', 'priority': 1,
+                          'transition': 'fade'})
+    self.slider.addSlide({'id': 2, 'duration': 3,
+                          'mode': 'layout', 'priority': 1,
+                          'transition': 'fade'})
+    self.slider.addSlide({'id': 3, 'duration': 3,
+                          'mode': 'layout', 'priority': 1,
+                          'transition': 'fade'})
 
   def testSlideCommands(self):
     self.slider.removeSlide(1)
     self.slider.removeSlide(2)
     self.slider.removeSlide(3)
-    self.assertTrue(self.slider.isEmpty())
-    self.slider.addSlide(1 ,1, 1)
-    self.assertFalse(self.slider.isEmpty())
+    self.assertTrue(slider.isEmpty(self.slider._slides))
+    self.slider.addSlide({'id': 1, 'duration': 3,
+                          'mode': 'layout', 'priority': 1,
+                          'transition': 'fade'})
+    self.assertFalse(slider.isEmpty(self.slider._slides))
 
   def testActivity(self):
     self.assertTrue(self.slider.isActive())
@@ -29,5 +37,7 @@ class SliderTest(TestCase):
     self.slider.removeSlide(2)
     self.slider.removeSlide(3)
     self.assertFalse(self.slider.isActive())
-    self.slider.addSlide(3, 3, 1)
+    self.slider.addSlide({'id': 3, 'duration': 3,
+                          'mode': 'layout', 'priority': 1,
+                          'transition': 'fade'})
     self.assertTrue(self.slider.isActive())
