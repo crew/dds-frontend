a73797f3dd716f2f48cdb6524e61534732214f93
Will Nowak <compbrain@gmail.com>
2009-08-05 10:37:35 -0400

    Convert config.py to use optionparser rather than eval

--- examples/config.py
+++
@@ -1,4 +0,0 @@
-config = { "cache" : "~/.dds/slides",
-           "client-jid" : "rms@dds-master.ccs.neu.edu/dds-client",
-           "client-password" : "foo",
-           "server-jid" : "test@dds-master.ccs.neu.edu/dds-server-init" }
