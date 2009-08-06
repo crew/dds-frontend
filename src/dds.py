66bdfd58ac37ea89e2a62a3312111a709c6020a2
Will Nowak <compbrain@gmail.com>
2009-08-05 21:11:42 -0400

    Switch the stages back, forgot that the non default stage hates fullscreen

--- src/dds.py
+++ src/dds.py
@@ -115,7 +115,7 @@ def main():
   initializeLibraries()
   logging.basicConfig(level=logging.DEBUG,
                       format='%(asctime)s %(levelname)s %(message)s')
-  stage = clutter.Stage()
+  stage = clutter.stage_get_default()
   config.init()
   setupCache()
   show = Slider(stage)
