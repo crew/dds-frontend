8be2ac30515189d87af05f8281b58d6de32a535f
Will Nowak <compbrain@gmail.com>
2010-01-29 14:48:56 -0500

    Merge common code into baseslide, remove script

--- src/baseslide.py
+++ src/baseslide.py
@@ -6,6 +6,9 @@ import clutter
 import gtk
 import logging
 import urllib2
+import htmlentitydefs
+import re
+
 
 class BaseSlide(object):
   """Base Slide Module."""
@@ -14,12 +17,24 @@ class BaseSlide(object):
     self.group = clutter.Group()
   
   def setupslide(self):
+    """Hook to be called before display on screen."""
     pass
 
   def teardownslide(self):
+    """Hook to be called after display on screen."""
     pass
 
   def TextureFromPixbuf(self, pixbuf, texture=None):
+    """Given a pixbuf, create a clutter texture or update an existing texture.
+
+    Args:
+       pixbuf: (gtk.gdk.Pixbuf) pixbuf to translate
+       texture (clutter.Texture) optionally update the given texture
+
+    Returns:
+      clutter.Texture on success, None on failure
+    """
+
     data   = pixbuf.get_pixels()
     width  = pixbuf.get_width()
     height = pixbuf.get_height()
@@ -36,6 +51,14 @@ class BaseSlide(object):
       return texture
 
   def PixbufFromData(self, data):
+    """Given some image data, get a Pixbuf.
+
+    Args:
+       data: bytestring of image (png/jpg/etc) data
+
+    Returns:
+       gtk.gdk.Pixbuf
+    """
     loader = gtk.gdk.PixbufLoader()
     loader.write(data)
     loader.close()
@@ -43,6 +66,14 @@ class BaseSlide(object):
     return pixbuf
 
   def GetDataFromURL(self, url):
+    """Open a URL and get its content data.
+
+    Args:
+       url: (string) HTTP URI
+
+    Returns:
+       data string on success, None on failure
+    """
     try:
       logging.info('Slide fetching data from %s' % url)
       u = urllib2.urlopen(url)
@@ -53,13 +84,48 @@ class BaseSlide(object):
       return None
 
   def GetTextureFromURL(self, url, texture=None):
+    """Given a URL, get a clutter.Texture from its data.
+
+    Args:
+       url: (string) HTTP URI
+       texture (clutter.Texture) optionally update the given texture
+
+    Returns:
+      clutter.Texture on success, None on failure
+    """
     data = self.GetDataFromURL(url)
     if not data:
-      return
+      return None
     pixbuf = self.PixbufFromData(data)
     if not pixbuf:
-      return
+      return None
     texture = self.TextureFromPixbuf(pixbuf, texture)
     if not texture:
-      return
+      return None
     return texture
+
+  def UnescapeHTMLEntities(self, data):
+    """Replace HTML entities with their unicode equivalent.
+
+    Args:
+       data: (string) text to unescape
+
+    Returns:
+       unescaped string
+    """
+    if '#39' not in htmlentitydefs.name2codepoint:
+      htmlentitydefs.name2codepoint['#39'] = 39
+    return re.sub('&(%s);' % '|'.join(htmlentitydefs.name2codepoint),
+                  lambda m: unichr(name2codepoint[m.group(1)]), data)
+
+  def RemoveHTMLTags(self, data):
+    """Remove HTML tags from the given data after unescaping.
+    Args:
+       data: (string) text to clean
+
+    Returns:
+       cleaned string
+    """
+    #FIXME: eww.
+    html_tag = re.compile(r'<.*?>')
+    return self.UnescapeHTMLEntities(html_tag.sub('', data))
