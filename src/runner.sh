d347178c074c7650a73b4e72e0d315fc8a3476dd
Will Nowak <compbrain@gmail.com>
2010-02-04 15:58:00 -0500

    Add DDS runner

---
+++ src/runner.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+origdir=`pwd`
+newdir=`dirname $0`
+host=`hostname -f`
+
+cd $newdir
+while true
+do
+logfile=`mktemp`
+./dds.py > $logfile
+cat $logfile|mail -s "DDS Crash on $host" ddscrashes@compbrain.net
+rm $logfile
+done
